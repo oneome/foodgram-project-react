4d20719ecf7c02789e22a74f39ffb35b710054b0
Прылипко Егор <Egorprylipko@mail.ru>
2023-06-29 00:01:01 +0300

    4

--- backend/foodgram/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Subscription
-
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'author')
-    search_fields = ('user', 'author')
