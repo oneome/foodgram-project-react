791fd60a7e29c66a1ede8e74351edd876d482a7f
Прылипко Егор <Egorprylipko@mail.ru>
2023-06-30 20:20:29 +0300

    9

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,10 +69,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 if DEBUG:
     DATABASES = {
-       'default': {
+        'default': {
            'ENGINE': 'django.db.backends.sqlite3',
            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-       }
+        }
     }
 else:
     DATABASES = {
