c42ed5b7ce18534c533d6692612583c37ecc539f
Прылипко Егор <Egorprylipko@mail.ru>
2023-07-01 19:01:35 +0300

    13

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -8,5 +8,5 @@ class IsAuthorOrAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and (
-                request.user.is_admin
-                or obj.author == request.user or request.method == 'POST')
+            request.user.is_admin
+            or obj.author == request.user or request.method == 'POST')
